6f035661cf569386f273d87c5d73cb80f3ad1fbc
barbaraishioka <barbara.ishioka@gmail.com>
2022-08-19 22:41:59 -0300

    tranformando dashboard da ong em componente

---
+++ src/components/HeaderDashboard/styles.js
@@ -0,0 +1,18 @@
+
+
+import styled from "styled-components";
+
+export const Header = styled.header`
+
+header {
+    width: 100%;
+    height: 9.4rem;
+    background-color: rgba(76, 149, 108, 0.9);
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    padding-inline: 8.2rem;
+} 
+
+`;
