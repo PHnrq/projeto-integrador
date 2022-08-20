8562141b765636bb3d34ee8bd49cf0341c445e07
PHnrq <paulofigueredo7@gmail.com>
2022-08-20 08:50:03 -0300

    Adiciona variaveis de cor

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -20,6 +20,18 @@ export const GlobalStyle = createGlobalStyle`
     --button-color: #D68C45;
     --dark-color: #153423;
     --footer-color: #09150E;
+    
+    --quinary-color: #FFC9B9;
+    --senary-color: #153423;
+    --dark-color: #000000;
+    --light-dark-color: #09150E;
+    --header-color: rgba(76, 149, 108, 0.9);
+    --arrow-btn-color: rgba(76, 149, 108, 0.25);
+    --other-color: rgba(76, 149, 108, 0.05);
+    --border-color: #9B9696;
+    --border2-color: #38463E;
+    --border3-color: #AFB1B6;
+    --shadow-color: rgba(0, 0, 0, 0.25);
 
     font-family: 'Poppins', sans-serif;
     font-size: 10px;
