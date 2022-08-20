50f01e8721c24214c76dd4c44839977b97120b82
PHnrq <paulofigueredo7@gmail.com>
2022-08-20 08:51:27 -0300

    Move as imagens para a pasta assets e corrige caminho

--- src/pages/DashboardONG/index.jsx
+++ src/pages/DashboardONG/index.jsx
@@ -1,163 +1,164 @@
 import React from "react";
-import {Header} from "../../components/HeaderDashboard/index";
-import {Container} from "./styles";
+import { Header } from "../../components/HeaderDashboard";
+import { Container } from "./styles";
 
-import bean from "./assets/bean.png";
-import buyAdd from "./assets/buy-add.png";
-import buyConfirm from "./assets/buy-confirm.png";
-import flour from "./assets/flour.png";
-import leftArrow from "./assets/left-arrow.png";
-import rice from "./assets/rice.png";
-import rightArrow from "./assets/right-arrow.png";
-import sugar from "./assets/sugar.png";
-import trash from "./assets/trash.png";
+import bean from "../../assets/bean.png";
+import buyAdd from "../../assets/buy-add.png";
+import buyConfirm from "../../assets/buy-confirm.png";
+import flour from "../../assets/flour.png";
+import leftArrow from "../../assets/left-arrow.png";
+import rice from "../../assets/rice.png";
+import rightArrow from "../../assets/right-arrow.png";
+import sugar from "../../assets/sugar.png";
+import trash from "../../assets/trash.png";
 
 export function DashboardOng() {
-    return (
-        <Container>
-            <Header />
+  return (
+    <Container>
+      <Header />
 
-            <main class="main">
+      <main className="main">
+        <div className="right-container">
+          <section className="find-donors">
+            <h2 className="find-donors-title">Ache os doadores mais próximos</h2>
 
-                <div class="right-container">
+            <div className="search-radius-container">
+              <label htmlFor="search-radius">Defina o raio de busca</label>
+              <input type="range" name="search-radius" id="search-radius" />
+            </div>
 
-                    <section class="find-donors">
+            <div className="map-and-donors-list">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6162.28090355021!2d-51.1622968501601!3d-23.311616892832973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94eb435f8940fd39%3A0xf9d4888545f0b415!2sCatedral%20Metropolitana%20de%20Londrina!5e0!3m2!1spt-BR!2sbr!4v1659648510301!5m2!1spt-BR!2sbr"
+                width="217"
+                height="150"
+                style={{border: 0}}
+                allowFullScreen=""
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+                title="Map"
+              ></iframe>
 
-                        <h2 class="find-donors-title">Ache os doadores mais próximos</h2>
+              <ul className="donors-list">
+                <li className="donors-list-item">Mercado Mão Amiga</li>
+                <li className="donors-list-item">Deskontão</li>
+                <li className="donors-list-item">João Silva</li>
+              </ul>
+            </div>
+          </section>
 
-                        <div class="search-radius-container">
-                            <label for="search-radius">Defina o raio de busca</label>
-                            <input type="range" name="search-radius" id="search-radius" />
-                        </div>
+          <section className="available-products">
+            <h2 className="available-products-title">
+              Produtos disponives em&nbsp;
+              <span className="donor-name">Mercado Mão Amiga</span>
+            </h2>
 
-                        <div class="map-and-donors-list">
-                            <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6162.28090355021!2d-51.1622968501601!3d-23.311616892832973!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94eb435f8940fd39%3A0xf9d4888545f0b415!2sCatedral%20Metropolitana%20de%20Londrina!5e0!3m2!1spt-BR!2sbr!4v1659648510301!5m2!1spt-BR!2sbr"
-                                width="217" height="150" style="border:0;" allowfullscreen="" loading="lazy"
-                                referrerpolicy="no-referrer-when-downgrade">
-                            </iframe>
+            <div className="see-all-container">
+              <button className="see-all">
+                Ver todos
+              </button>
+            </div>
 
-                            <ul class="donors-list">
-                                <li class="donors-list-item">Mercado Mão Amiga</li>
-                                <li class="donors-list-item">Deskontão</li>
-                                <li class="donors-list-item">João Silva</li>
-                            </ul>
+            <div className="partial-product-list">
+              <button className="btn-arrow">
+                <img src={rightArrow} alt="right-arrow" />
+              </button>
 
-                        </div>
+              <ul className="product-list">
+                <li className="product-list-item">
+                  <div className="product-item-date">
+                    <img src={rice} alt="rice" className="list-item-img" />
+                    <p className="list-item-validity">VAL: 13 / 08 / 2022</p>
+                  </div>
 
-                    </section>
+                  <p className="list-item-name">Arroz Branco</p>
 
-                    <section class="available-products">
-                        <h2 class="available-products-title">Produtos disponives em&nbsp;<span class="donor-name">Mercado
-                            Mão
-                            Amiga</span></h2>
+                  <div className="buy-add-icon-container">
+                    <button className="buy-add-icon">
+                      <img src={buyAdd} alt="buy-add-icon" />
+                    </button>
+                  </div>
+                </li>
 
-                        <div class="see-all-container">
-                            <a href="#" class="see-all">Ver todos</a>
-                        </div>
+                <li className="product-list-item">
+                  <div className="product-item-date">
+                    <img src={sugar} alt="sugar" className="list-item-img" />
+                    <p className="list-item-validity">VAL: 13 / 08 / 2022</p>
+                  </div>
 
-                        <div class="partial-product-list">
+                  <p className="list-item-name">Açucar Cristal</p>
 
-                            <button class="btn-arrow"><img src={rightArrow} alt="right-arrow" /></button>
+                  <div className="buy-add-icon-container">
+                    <button className="buy-add-icon">
+                      <img src={buyAdd} alt="buy-add-icon" />
+                    </button>
+                  </div>
+                </li>
 
-                            <ul class="product-list">
+                <li className="product-list-item">
+                  <div className="product-item-date">
+                    <img src={bean} alt="bean" className="list-item-img" />
+                    <p className="list-item-validity">VAL: 13 / 08 / 2022</p>
+                  </div>
 
-                                <li class="product-list-item">
-                                    <div class="product-item-date">
-                                        <img src={rice} alt="rice" class="list-item-img" />
-                                        <p class="list-item-validity">VAL: 13 / 08 / 2022</p>
-                                    </div>
+                  <p className="list-item-name">Feijão Carioca</p>
 
-                                    <p class="list-item-name">Arroz Branco</p>
+                  <div className="buy-add-icon-container">
+                    <button className="buy-add-icon">
+                      <img src={buyAdd} alt="buy-add-icon" />
+                    </button>
+                  </div>
+                </li>
+              </ul>
 
-                                    <div class="buy-add-icon-container">
-                                        <button class="buy-add-icon"><img src={buyAdd}
-                                            alt="buy-add-icon" /></button>
-                                    </div>
+              <button className="btn-arrow">
+                <img src={leftArrow} alt="left-arrow" />
+              </button>
+            </div>
+          </section>
+        </div>
 
+        <div className="left-container">
+          <form className="demand-form">
+            <h2 className="demand-title">Meu pedido</h2>
 
-                                </li>
-
-                                <li class="product-list-item">
-                                    <div class="product-item-date">
-                                        <img src={sugar} alt="sugar" class="list-item-img" />
-                                        <p class="list-item-validity">VAL: 13 / 08 / 2022</p>
-                                    </div>
-
-                                    <p class="list-item-name">Açucar Cristal</p>
-
-                                    <div class="buy-add-icon-container">
-                                        <button class="buy-add-icon"><img src={buyAdd}
-                                            alt="buy-add-icon" /></button>
-                                    </div>
-                                </li>
-
-                                <li class="product-list-item">
-                                    <div class="product-item-date">
-                                        <img src={bean} alt="bean" class="list-item-img" />
-                                        <p class="list-item-validity">VAL: 13 / 08 / 2022</p>
-                                    </div>
-
-                                    <p class="list-item-name">Feijão Carioca</p>
-
-                                    <div class="buy-add-icon-container">
-                                        <button class="buy-add-icon"><img src={buyAdd}
-                                            alt="buy-add-icon" /></button>
-                                    </div>
-                                </li>
-
-                            </ul>
-
-                            <button class="btn-arrow"><img src={leftArrow} alt="left-arrow" /></button>
-
-                        </div>
-                    </section>
+            <div className="donor-demand-container">
+              <p className="donor-demand-name">Mercado Mão Amiga</p>
+              <div className="donor-demand-others-data">
+                <div className="item-img-container">
+                  <img src={flour} alt="flour" className="item-img" />
                 </div>
-
-                <div class="left-container">
-
-                    <form action="" class="demand-form">
-
-                        <h2 class="demand-title">Meu pedido</h2>
-
-                        <div class="donor-demand-container">
-                            <p class="donor-demand-name">Mercado Mão Amiga</p>
-                            <div class="donor-demand-others-data">
-                                <div class="item-img-container">
-                                    <img src={flour} alt="flour" class="item-img" />
-                                </div>
-                                <div class="item-name-qtd-container">
-                                    <p class="demand-item-name">Farinha de trigo</p>
-                                    <div class="qtd-sign-trash-container">
-                                        <span class="qtd">QTD</span>
-                                        <div class="sign-value-container">
-                                            <button class="minus-sign">-</button>
-                                            <span class="qtd-value">2</span>
-                                            <button class="plus-sign">+</button>
-                                        </div>
-                                        <button class="trash-icon"><img src={trash} alt="trash-icon" /></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="demand-get-date">
-                            <p class="demand-text">Selecione a data de retirada</p>
-                            <input type="date" name="get-date" id="get-date" />
-                        </div>
-
-                    </form>
-
-                    <div class="submit-btn-container">
-                        <button type="submit" class="submit-btn"><img src={buyConfirm}
-                            alt="buy-confirm-icon" />&nbsp;Finalizar Pedido</button>
+                <div className="item-name-qtd-container">
+                  <p className="demand-item-name">Farinha de trigo</p>
+                  <div className="qtd-sign-trash-container">
+                    <span className="qtd">QTD</span>
+                    <div className="sign-value-container">
+                      <button className="minus-sign">-</button>
+                      <span className="qtd-value">2</span>
+                      <button className="plus-sign">+</button>
                     </div>
-
+                    <button className="trash-icon">
+                      <img src={trash} alt="trash-icon" />
+                    </button>
+                  </div>
                 </div>
+              </div>
+            </div>
 
-            </main>
+            <div className="demand-get-date">
+              <p className="demand-text">Selecione a data de retirada</p>
+              <input type="date" name="get-date" id="get-date" />
+            </div>
+          </form>
 
-        </Container>
-
-    )
+          <div className="submit-btn-container">
+            <button type="submit" className="submit-btn">
+              <img src={buyConfirm} alt="buy-confirm-icon" />
+              &nbsp;Finalizar Pedido
+            </button>
+          </div>
+        </div>
+      </main>
+    </Container>
+  );
 }
