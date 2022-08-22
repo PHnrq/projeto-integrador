dad9192f972f2e33497687a94499e098489a2bce
PHnrq <paulofigueredo7@gmail.com>
2022-08-22 09:33:33 -0300

    Corrige bugs na estilização

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,25 +1,35 @@
 import React from "react";
-import {Container} from "./styles";
-import logo from "../../assets/logo.svg";
+import { Container } from "./styles";
+import logo from "../../assets/logoLg.svg";
 import vetorLogin from "../../assets/vector-login.png";
 
 export function Login() {
   return (
-    <Container className="container">
+    <Container>
       <div className="main-left">
-        <img className="main-left__logo" src={logo} />
-        <h3 className="main-left__description">Conectamos  doadores às pessoas em insuficiência  alimentar
-          e ONGs com o objetivo de reduzir o desperdício de alimentos.</h3>
+        <img className="main-left__logo" src={logo} alt="" />
+        <h3 className="main-left__description">
+          Conectamos doadores às pessoas em insuficiência alimentar e ONGs com o
+          objetivo de reduzir o desperdício de alimentos.
+        </h3>
       </div>
       <form className="main-right__login form" action="#" method="post">
-        <label for="email"></label>
-        <input type="text" className="main-right__login__input" placeholder="E-mail" />
-        <label for="password"></label>
-        <input type="password" className="main-right__login__input" placeholder="Senha" />
-        <button className="btn btn-login" type="submit" />
-        <img src={vetorLogin} />
-        <p className="btn-text"><button>Login</button></p>
-        <a href="#" className="main-right__login__link">Esqueceu sua senha?</a>
+        <label htmlFor="email"></label>
+        <input
+          type="text"
+          className="main-right__login__input"
+          placeholder="E-mail"
+        />
+        <label htmlFor="password"></label>
+        <input
+          type="password"
+          className="main-right__login__input"
+          placeholder="Senha"
+        />
+        <button className="btn btn-login" type="submit">
+          <img src={vetorLogin} alt="" />
+          <p className="btn-text">Login</p>
+        </button>
         <hr className="line" />
         <p className="main-right__register">Novo(a) por aqui?</p>
         <button className="btn btn-register btn-text">Cadastre-se</button>
