70150bb4d4369f30d45fb3244f3a383d35781748
PHnrq <paulofigueredo7@gmail.com>
2022-08-31 23:52:07 -0300

    Adiciona Card do pedido

--- src/components/CardProdutosPedido/index.jsx
+++ src/components/CardProdutosPedido/index.jsx
@@ -1,30 +1,49 @@
-import React from "react";
-import trash from "../../assets/trash.png";
-import flour from "../../assets/flour.png";
+import{ useState} from 'react'
 
+import { Container } from "./styles";
 
+import placeholderImg from "../../assets/placeholder-Img.svg";
+import trashImg from "../../assets/trash.png";
 
-export function CardProduto ({nameProduct, amount}) {
-    return (
-            <div className="donor-demand-others-data">
-                <div className="item-img-container">
-                  <img src={flour} alt="flour" className="item-img" />
-                </div>
-                <div className="item-name-qtd-container">
-                  <p className="demand-item-name">{nameProduct}</p>
-                  <div className="qtd-sign-trash-container">
-                    <span className="qtd">QTD</span>
-                    <div className="sign-value-container">
-                      <button className="minus-sign">-</button>
-                      <span className="qtd-value">{amount}</span>
-                      <button className="plus-sign">+</button>
-                    </div>
-                    <button className="trash-icon">
-                      <img src={trash} alt="trash-icon" />
-                    </button>
-                  </div>
-                </div>
-            </div>
-            
-            )
+//Receber como paramentro todas a propriedades necessarias.
+export function CardProduto({amount, nameProduct, index, handleRemoveProduct, handleUpdateAmount}) {
+  const [productAmount, setProductAmount] = useState(parseInt(amount))
+
+  function handleAmountDecrease() {
+    if(productAmount <= 1){
+      return
+    }
+
+    setProductAmount(productAmount - 1)
+
+    handleUpdateAmount(productAmount, index)
+  }
+
+  function handleAmountIncrease() {
+    setProductAmount(productAmount + 1)
+    handleUpdateAmount(productAmount, index)
+  }
+
+  return (
+    <Container>
+      <div>
+        <img src={placeholderImg} alt=""/>
+      </div>
+      <p className="product-name">{nameProduct}</p>
+
+      <div className="add-decrease-remove-wrapper">
+        <div>
+          <div className="amount__modifier">
+            <button onClick={() => handleAmountDecrease()}>-</button>
+            <span>{productAmount}</span>
+            <button onClick={() => handleAmountIncrease()}>+</button>
+          </div>
+        </div>
+
+        <button onClick={() => handleRemoveProduct(index)}>
+          <img src={trashImg} alt="trash-icon" />
+        </button>
+      </div>
+    </Container>
+  );
 }
